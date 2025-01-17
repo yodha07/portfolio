3a78462ee8c0feed15b09e9ebc8edbf0e640667c
yodha07 <sutaraditya15@gmail.com>
2025-01-17 21:05:00 +0530

    commit

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -55,7 +55,7 @@ const ProjectInfo = () => (
 );
 
 export default function Project() {
-    window.location.reload(1)
+    location.reload()
     window.scrollTo({top: 0, behavior:'smooth'})
     console.log("Working")
     return(
